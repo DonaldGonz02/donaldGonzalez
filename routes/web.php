0a4bd7ac0c020ed4639b5d27f226d602cb1a5941
DonaldGonz02 <rexgrady16@gmail.com>
2024-11-03 10:22:32 -0600

    View Blog!
    Se agrega la Vista principal, Blog

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/blog', [App\Http\Controllers\BlogController::class, 'index'])->name('blog');
